0d1a334c7559e38acf9f0998290547223e9d1e3c
Roman Khomenko <romankhomenko1995@gmail.com>
2020-08-31 22:01:39 +0300

    Template added

---
+++ src/main/java/org/khomenko/homework/ex/one/DynamicArray.java
@@ -0,0 +1,136 @@
+package org.khomenko.homework.ex.one;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class DynamicArray<T> implements List<T> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<T> operator) {
+
+    }
+
+    @Override
+    public void sort(Comparator<? super T> c) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int i) {
+        return null;
+    }
+
+    @Override
+    public T set(int i, T t) {
+        return null;
+    }
+
+    @Override
+    public void add(int i, T t) {
+
+    }
+
+    @Override
+    public T remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return null;
+    }
+}
---
+++ src/test/java/org/khomenko/homework/ex/one/DynamicArrayTest.java
@@ -0,0 +1,11 @@
+package org.khomenko.homework.ex.one;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class DynamicArrayTest {
+    @Test
+    public void someTestMethod() {
+        Assert.assertEquals(0, 0);
+    }
+}
