fa7b876cec643f41fcabd67031e86733dd404320
Roman Khomenko <romankhomenko1995@gmail.com>
2020-09-01 22:58:55 +0300

    Some changes

---
+++ src/main/java/org/khomenko/homework/ex/one/Iterator.java
@@ -0,0 +1,15 @@
+package org.khomenko.homework.ex.one;
+
+import java.util.Iterator;
+
+public class DynamicArrayIterator<T> implements Iterator<T> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+}
