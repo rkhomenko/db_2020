8ffa603b1cb943c90fe8a5e86024ca080cdce9cf
Roman Khomenko <romankhomenko1995@gmail.com>
2020-09-10 13:22:20 +0300

    Fixed MyService annotation

--- src/main/java/homework/database/service/MyService.java
+++ src/main/java/homework/database/service/MyService.java
@@ -2,9 +2,9 @@ package homework.database.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class MyService {
     @Autowired
     @Derby
